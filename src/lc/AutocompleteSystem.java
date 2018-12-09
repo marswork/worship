47b4d215f785e027e770fad872652c968f317cf6
Marshall Hung <wshung@yahoo-inc.com>
2018-12-09 16:38:05 +0800

    autocomplete system optimized

--- src/lc/AutocompleteSystem.java
+++ src/lc/AutocompleteSystem.java
@@ -6,6 +6,36 @@ import java.util.List;
 import java.util.PriorityQueue;
 import javafx.util.Pair;
 
+/**
+ * LC 642 Design Search Autocomplete System
+ *
+ * 在 TrieNode 上紀錄這個點為終點的 sentence times 是多少
+ *
+ * insert:
+ * 就照一般方式來做，不同在於結尾是塞 times
+ *
+ * input:
+ * 使用一個 TrieNode cur 來記錄目前走到哪個 TrieNode
+ * 遇到 "#" 就在當前 cur.times++
+ * 其他則做 dfs，找這個 node 以下所有的 path，
+ * 只要 times > 0 的 node 就代表有被搜尋過，這些會使用 Top K 算法 (min heap O(NlogK))
+ *
+ * 跟官方詳解比較起來：
+ * 我優化的部分在於 input 時
+ * 1. 用 TrieNode cur，就不用每次都重新再從開頭重新搜一遍
+ * 2. 用 StringBuilder 不用建一堆 String
+ *
+ * 如何再優化?
+ * 1. 把每個 node 的 top K string 存進 node，但這會很耗 space
+ * 2. 把每個 node 的 top K 最後一個 TrieNode 存進 node，每個 TrieNode 要多加一個 prev 已便還原原本的 sentence
+ *
+ * 優化方法如何 update 新的 times 到所有關聯的 node?
+ * 每個 node 做 postorder traversal，每條路都回傳一個 List<Pair<TrieNode, Integer>>
+ * 比較當前這個 node 所記錄的 top 跟回傳的 List 看看
+ * 1. 有沒有人原本就在 top3 而 times 值需要更新
+ * 2. 有沒有人原本沒在 top3，但打敗前面的人成為新的 top3
+ */
+
 public class AutocompleteSystem {
     public static void main(String[] args) {
         String[] sentences = {"i love you", "island", "ironman", "i love leetcode"};
@@ -33,24 +63,26 @@ public class AutocompleteSystem {
         for (int i = 0; i < sentences.length; i++) {
             insert(root, sentences[i], times[i]);
         }
+        cur = root;
     }
+    private TrieNode cur;
     private StringBuilder sb = new StringBuilder();
     public List<String> input(char c) {
         System.out.println("input: " + c);
         List<String> res = new LinkedList<>();
         if (c == '#') {
-            insert(root, sb.toString(), 1);
+            cur.times++;
             sb.setLength(0);
+            cur = root;
         } else {
-            TrieNode cur = root;
             sb.append(c);
-            for (int i = 0; i < sb.length(); i++) {
-                char sbc =  sb.charAt(i);
-                int idx = sbc == ' ' ? 26 : sbc - 'a';
-                if (cur.next[idx] == null)
-                    return Collections.emptyList();
+            int idx = c == ' ' ? 26 : c - 'a';
+            if (cur.next[idx] == null) {
+                cur.next[idx] = new TrieNode();
                 cur = cur.next[idx];
+                return Collections.emptyList();
             }
+            cur = cur.next[idx];
             PriorityQueue<Pair<String, Integer>> pq = new PriorityQueue<>(
                     (s1, s2) -> s1.getValue() - s2.getValue()
             );
