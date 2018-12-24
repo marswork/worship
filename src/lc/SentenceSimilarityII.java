b2afd4a5f3af49762cc5b185b7f7987c40910ff3
Marshall Hung <wshung@yahoo-inc.com>
2018-12-24 15:53:57 +0800

    sentence similarity II dfs

--- src/lc/SentenceSimilarityII.java
+++ src/lc/SentenceSimilarityII.java
@@ -1,6 +1,8 @@
 package lc;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class SentenceSimilarityII {
@@ -15,6 +17,7 @@ public class SentenceSimilarityII {
         };
         SentenceSimilarityII s = new SentenceSimilarityII();
         System.out.println(s.areSentenceSimilar(words1, words2, pairs));
+        System.out.println(s.areSentenceSimilar2(words1, words2, pairs));
     }
     public boolean areSentenceSimilar(String[] words1, String[] words2, String[][] pairs) {
         if (words1.length != words2.length)
@@ -42,5 +45,41 @@ public class SentenceSimilarityII {
             parents.put(s, find(parents.get(s), parents));
         }
         return parents.get(s);
+    }
+    public boolean areSentenceSimilar2(String[] words1, String[] words2, String[][] pairs) {
+        if (words1.length != words2.length)
+            return false;
+        int n = words1.length;
+        Map<String, List<String>> graph = new HashMap<>();
+        for (String[] pair : pairs) {
+            graph.putIfAbsent(pair[0], new ArrayList<>());
+            graph.putIfAbsent(pair[1], new ArrayList<>());
+            graph.get(pair[0]).add(pair[1]);
+            graph.get(pair[1]).add(pair[0]);
+        }
+        int color = 0;
+        Map<String, Integer> colors = new HashMap<>();
+        for (String[] pair : pairs) {
+            if (!colors.containsKey(pair[0]))
+                dfs(graph, pair[0], color++, colors);
+        }
+        for (int i = 0; i < n; i++) {
+            String word1 = words1[i], word2 = words2[i];
+            if (word1.equals(word2))
+                continue;
+            System.out.println("word1:" + word1 + " color:" + colors.get(word1));
+            System.out.println("word2" + word2 + " color:" + colors.get(word2));
+            if (!colors.containsKey(word1) || !colors.containsKey(word2) ||
+                    !colors.get(word1).equals(colors.get(word2)))
+                return false;
+        }
+        return true;
+    }
+    private void dfs(Map<String, List<String>> graph, String cur, int color, Map<String, Integer> colors) {
+        colors.put(cur, color);
+        for (String nei : graph.get(cur)) {
+            if (!colors.containsKey(nei))
+                dfs(graph, nei, color, colors);
+        }
     }
 }
